614513f6f1e54419e2c2f7d87e767d42b13099ce
Lindomário D. <lindomariodamasceno@gmail.com>
2026-03-13 15:59:45 -0300

    alteração no teste lint

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+const reactPlugin = require("eslint-plugin-react");
+const globals = require("globals");
+
+module.exports = [
+  {
+    files: ["**/*.{js,jsx}"],
+    plugins: {
+      react: reactPlugin,
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      "no-undef": "error",
+      "react/react-in-jsx-scope": "off",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+];
